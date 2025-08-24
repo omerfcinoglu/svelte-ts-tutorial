d61b3a19f2fac82eb50b0c6ce3d5fb021aaf01a2
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-08-24 17:40:18 +0300

    Add new pages

---
+++ src/lib/pages/Page2.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let title: string = "Page 2";
+</script>
+
+<div class="page">
+  <h1>{title}</h1>
+</div>
+
+<style>
+  .page {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
